feee48e24222421adfb6a23dfd8a9b06dee5d934
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2020-07-12 10:37:03 +0900

    Add LinearLayoutActivity

--- app/src/main/java/com/asterlist/androidsamples/MainActivity.kt
+++ app/src/main/java/com/asterlist/androidsamples/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.container, MainFragment())
+        transaction.replace(R.id.container, MainFragment())
         transaction.commit()
     }
 }
---
+++ app/src/main/java/com/asterlist/androidsamples/layout/LinearLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.asterlist.androidsamples.layout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asterlist.androidsamples.R
+
+class LinearLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_linear)
+    }
+}
