9b4eacc1837519302d367da9737757561d8c72a6
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2020-07-21 20:57:00 +0900

    Update CameraCalibrator

--- app/src/main/java/com/asterlist/android/opencv/cameracalibration/CameraCalibrator.kt
+++ app/src/main/java/com/asterlist/android/opencv/cameracalibration/CameraCalibrator.kt
@@ -1,5 +1,6 @@
 package com.asterlist.android.opencv.cameracalibration
 
+import org.opencv.calib3d.Calib3d
 import org.opencv.core.Size
 
 class CameraCalibrator {
@@ -7,9 +8,15 @@ class CameraCalibrator {
         private const val TAG = "OCV::CameraCalibrator"
     }
 
+    private var mFlags = 0
     private var mImageSize: Size? = null
 
     constructor(width: Int, height: Int) {
         mImageSize = Size(width.toDouble(), height.toDouble())
+        mFlags = Calib3d.CALIB_FIX_PRINCIPAL_POINT +
+                Calib3d.CALIB_ZERO_TANGENT_DIST +
+                Calib3d.CALIB_FIX_ASPECT_RATIO +
+                Calib3d.CALIB_FIX_K4 +
+                Calib3d.CALIB_FIX_K5
     }
 }
