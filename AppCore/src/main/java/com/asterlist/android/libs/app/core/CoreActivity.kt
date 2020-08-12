724b9cf9900330bf35d58aed205bc4a8bbbea1c5
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2020-08-13 08:37:10 +0900

    Add CoreActivity

---
+++ AppCore/src/main/java/com/asterlist/android/libs/app/core/CoreActivity.kt
@@ -0,0 +1,47 @@
+package com.asterlist.android.libs.app.core
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class CoreActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = "CoreActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (BuildConfig.DEBUG) {Log.d(TAG, "onCreate")}
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (BuildConfig.DEBUG) {Log.d(TAG, "onStart")}
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (BuildConfig.DEBUG) {Log.d(TAG, "onResume")}
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (BuildConfig.DEBUG) {Log.d(TAG, "onPause")}
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (BuildConfig.DEBUG) {Log.d(TAG, "onStop")}
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (BuildConfig.DEBUG) {Log.d(TAG, "onDestroy")}
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (BuildConfig.DEBUG) {Log.d(TAG, "onSaveInstanceState")}
+    }
+}
