900abed7a3e4a4fc94c36cc91f13d6eeb45c6a0a
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2020-09-01 08:49:01 +0900

    Add Data java

---
+++ src/main/java/com/asterlist/sample/java/Data.java
@@ -0,0 +1,11 @@
+package com.asterlist.sample.java;
+
+public class Data {
+    private String key;
+    private String value;
+
+    public Data(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
