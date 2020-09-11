d4e2d7fd759298f7070d1e1c3e010112188cf79e
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2020-09-11 19:31:02 +0900

    Update ForegroundActivity

--- app/src/main/java/com/asterlist/androidsamples/service/foreground/ForegroundActivity.kt
+++ app/src/main/java/com/asterlist/androidsamples/service/foreground/ForegroundActivity.kt
@@ -1,7 +1,11 @@
 package com.asterlist.androidsamples.service.foreground
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class ForegroundActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
