966bbfe58bbac65fbbfbf0a41e58c09256352b30
Yuya Honda <64887971+yuyahnd@users.noreply.github.com>
2021-01-01 15:20:26 +0900

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
+    install_requires=_requires_from_file('requirements.txt'),
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "pytest-cov"]
 )
